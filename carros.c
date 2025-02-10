391ccaf4ba6044e272c00493709bd4a1af11cba7
rodrigolyrio <rodrigolyrio2009@gmail.com>
2025-02-10 16:16:13 -0300

    Create carros.c

---
+++ carros.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    int kmx, kmy, vx, vy;
+    int h = 0; // Hora
+
+    printf("Digite a posição inicial do carro X (kmx): ");
+    scanf("%d", &kmx);
+    printf("Digite a velocidade do carro X (vx): ");
+    scanf("%d", &vx);
+    printf("Digite a posição inicial do carro Y (kmy): ");
+    scanf("%d", &kmy);
+    printf("Digite a velocidade do carro Y (vy): ");
+    scanf("%d", &vy);
+
+    if (vx == vy) {
+        printf("A velocidade dos carros devem ser diferentes entre si.\n");
+        return 1; // Erro
+    }
+
+    printf("Hora %d: Carro X em %d e Carro Y em %d\n", h, kmx, kmy); 
+
+    while ((vx > vy && kmx <= kmy) || (vx < vy && kmy <= kmx)) { // Enquanto não houver ultrapassagem entre os carros, o loop ocorrerá
+        h++;
+        kmx += vx; // Atualiza a posição do carro X
+        kmy += vy; // Atualiza a posição do carro Y
+
+        printf("Hora %d: Carro X em %d e Carro Y em %d\n", h, kmx, kmy);
+    }
+
+    if (kmx > kmy) {
+        printf("Carro X ultrapassou o Carro Y na hora %d após o KM %d\n", h, kmy);
+    } else {
+        printf("Carro Y ultrapassou o Carro X na hora %d após o KM %d\n", h, kmx);
+    }
+
+    return 0;
+}
