38d1bfa19d32700c0dc34eb25f7841598b92a7a5
rodrigolyrio <rodrigolyrio2009@gmail.com>
2025-02-13 01:19:08 -0300

    Create carros.asm

---
+++ carros.asm
@@ -0,0 +1,32 @@
+org 100h
+
+mov dx, offset msg1
+mov ah, 9
+int 21h 
+;call ler_valor / achar a funcao de ler num
+;mov [a] registradorx / por o registrador usado na funcao
+
+mov dx, offset msg2
+mov ah, 9
+int 21h
+;call ler_valor / achar a funcao de ler num
+;mov [a] registradorx / por o registrador usado na funcao
+
+mov dx, offset msg3
+mov ah, 9
+int 21h
+;call ler_valor / achar a funcao de ler num
+;mov [a] registradorx / por o registrador usado na funcao
+
+ret
+
+msg dw "digite o valor de a: $"
+msg dw "digite o valor de b: $"
+msg dw "digite o valor de c: $"
+msg dw "soma dos dois maiores: $" 
+
+; variaveis
+
+a dw ?
+b dw ?
+c dw ?
